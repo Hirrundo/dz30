cdecaebf137c27843ff083ebfbe06ef72fedc354
liza <lizzkalizzka@inbox.ru>
2026-08-20 20:23:53 +0300

    дз31

---
+++ dist/tasks.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function formatTasks(tasks, category) {
+    const taskList = tasks.join(", ");
+    if (category) {
+        return `${category}: ${taskList}`;
+    }
+    return taskList;
+}
+
+const tasks = [
+    "Сделать домашку",
+    "Погулять",
+    "Купить продукты"
+];
+
+const result1 = formatTasks(tasks);
+console.log(`Задачи: ${result1}`);
+
+const result2 = formatTasks(tasks, "На сегодня");
+console.log(`Задачи: ${result2}`);
---
+++ tasks.ts
@@ -0,0 +1,28 @@
+
+function formatTasks(tasks: string[]): string;
+
+function formatTasks(tasks: string[], category: string): string;
+
+
+function formatTasks(tasks: string[], category?: string): string {
+    const taskList = tasks.join(", ");
+
+    if (category) {
+        return `${category}: ${taskList}`;
+    }
+
+    return taskList;
+}
+
+const tasks: string[] = [
+    "Сделать домашку",
+    "Погулять",
+    "Купить продукты",
+    
+];
+
+const result1 = formatTasks(tasks);
+console.log(`Задачи: ${result1}`);
+
+const result2 = formatTasks(tasks, "На сегодня");
+console.log(`Задачи: ${result2}`);
